b4884ba0a7feb5d50c1172b9758a72e3704ef70b
TheUnusualSuspect <jorgen_buholm_johansen@hotmail.com>
2022-02-06 17:16:02 +0100

    fetching users

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'The Best dating app on the market';
+  users: any;
+  apiUrl: string = "https://localhost:5001/api/users";
+
+  constructor(private http: HttpClient) {}
+
+
+  ngOnInit(){
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get(this.apiUrl).subscribe(
+      data => {
+        this.users = data;
+      },
+      error => {
+        console.log(error)
+      }
+    );
+  }
 }
