76615ac18276fe4b59de35fedcc77cc64421e30d
TheUnusualSuspect <jorgen_buholm_johansen@hotmail.com>
2022-02-06 16:00:59 +0100

    Initial commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+//Endre til API.Data?
+//ctrl + . for å generere konstruktører og lignende
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
