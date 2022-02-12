12f15345c1f805c302b0890b9881c8955df50ca9
TheUnusualSuspect <jorgen_buholm_johansen@hotmail.com>
2022-02-12 18:09:01 +0100

    Added buggycontroller

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,48 @@
+using API.Controllers;
+using API.Data;
+using Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers
+{
+
+    public class BuggyController : BaseApiController
+    {
+        public DataContext _context { get; }
+        public BuggyController(DataContext context)
+        {
+            context = _context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+            if(thing == null) return NotFound();
+
+            return Ok(thing);
+        }
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var thing = _context.Users.Find(-1);
+
+            var thingToReturn = thing.ToString();
+
+            return thingToReturn;
+        }
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("Bad request!");
+        }
+
+    }
+}
