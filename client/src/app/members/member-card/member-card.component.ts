55a38b768ee526578feaa8d00dcf0606cd12aa62
TheUnusualSuspect <jorgen_buholm_johansen@hotmail.com>
2022-02-15 17:06:47 +0100

    UI improvement and card layout

---
+++ client/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrls: ['./member-card.component.css'],
+})
+export class MemberCardComponent implements OnInit {
+
+  @Input() member: Member;
+  dummyPicture: string = 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
