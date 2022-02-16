7daeef9b2224159c5bb09b314bf47429277a6c77
TheUnusualSuspect <jorgen_buholm_johansen@hotmail.com>
2022-02-16 13:59:49 +0100

    jwt interceptor and member details

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 
 
 @NgModule({
@@ -12,11 +14,13 @@ import { ToastrModule } from 'ngx-toastr';
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
-    })
+    }),
+    TabsModule.forRoot()
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
+    TabsModule,
   ]
 })
 export class SharedModule { }
