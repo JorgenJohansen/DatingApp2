c86868a849c570740673c2100541953cdef09514
TheUnusualSuspect <jorgen_buholm_johansen@hotmail.com>
2022-02-21 15:55:11 +0100

    sorting last active and created members

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -22,5 +22,7 @@ namespace Helpers
         public int MinAge { get; set; } = 18;
 
         public int MaxAge { get; set; } = 99;
+
+        public string OrderBy { get; set; } = "lastActive";
     }
 }
